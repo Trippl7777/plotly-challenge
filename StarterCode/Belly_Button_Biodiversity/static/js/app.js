6566a3c54daec52a56fa57c557e1853500b6f4d5
Jack <trippl7777@gmail.com>
2019-10-20 16:21:33 -0400

    initiall push

---
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -0,0 +1,96 @@
+function buildMetadata(sample) {
+  // @TODO: Complete the following function that builds the metadata panel
+  // Use `d3.json` to fetch the metadata for a sample
+  var url = `/metadata/${sample}`;
+
+  d3.json(url).then(function(response){
+    // if (error) return console.log(error);
+    var data = response;
+    var holder = d3.select("#sample-metadata");
+    // .html("") to clear any metadata
+    holder.html("");
+
+    Object.entries(data).forEach(([key, value]) => {
+      holder.append("p")
+          .text(`${key}:${value}`);
+    });
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+  });
+}
+
+
+function buildCharts(sample) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url = `/samples/${sample}`;
+  d3.json(url).then(function (response){
+    var x = response["otu_ids"];
+    var y = response["sample_values"];
+    var size = response["sample_values"];
+    var otus = response["otu_labels"];
+    // @TODO: Build a Bubble Chart using the sample data
+    var trace1 = {
+      x: x,
+      y: y,
+      mode:"markers",
+      marker:{
+        size: size,
+        color: x,
+        colorscale: "Rainbow", //color scale get from Alex
+        labels: otus,
+        type: 'scatter',
+        opacity: 0.5
+      }
+    };
+
+    var data1 = [trace1];
+
+    var layout = {
+      title: 'Size',
+      xaxis: { title: 'OTU acro' },
+      showlegend: true
+    };
+    Plotly.newPlot("bubble", data1, layout);
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+
+    var data = [{
+      values: size.splice(0, 10),
+      labels: x.splice(0, 10),
+      text: y.splice(0,10),
+      type: 'pie'
+    }];
+    Plotly.newPlot('pie', data);
+  });
+}
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // not working. dont know why -- come back to later
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+//fix so chart doesnt keep stacking
+function optionChanged(newSample) {
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
